aa041cd0f75309ae027a4e803c2f29854786f3eb
aman tiwari <amantiwari12081999@gmail.com>
2019-06-30 12:52:35 +0530

    some styling added

--- src/components/body.js
+++ src/components/body.js
@@ -3,14 +3,20 @@ import './body.css'
 
 const Body = () => {
   return <div className='b1 container card'>
-          <button className='b2 btn btn-sm btn-info'>Sign In</button>
-            <h6 className='b3 text-center'>Or</h6>
-         <button className='b4 btn btn-sm btn-info'>Sign Up</button>
-            <h5 className='b5 text-center'> Go with <i className='b8 fa fa-hand-o-down'></i></h5>
-            <span className='text-center'>
-              <i className='b6 fa fa-google'></i>
-              <i className='b7 fa fa-facebook-official'></i>
-            </span>
+
+          <div className='text-center'>
+                <button className=' b2 btn btn-sm shadow'>Sign In</button>
+                <h6 className='b3'>Or</h6>
+                <button className='b4 btn btn-sm shadow'>Sign Up</button>
+         </div>
+
+          <div className='text-center'>
+            <h5 className='b5'> Continue with <i className='fa fa-hand-o-down'></i></h5>
+              <span>
+                <i className='b6 fa fa-google'></i>
+                <i className='b7 fa fa-facebook-official'></i>
+             </span>
+         </div>
          </div>
 }
 export default Body;
